3a038402a92a981adc536c1ce1a5004251bc4d9c
“Neill“ <olazabalneill2004@gmail.com>
2025-07-13 01:22:40 -0500

    DDD + MVC + ORM

---
+++ src/main/java/com/unsa/bolsalaboral/BolsaLaboralApplication.java
@@ -0,0 +1,13 @@
+package com.unsa.bolsalaboral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaLaboralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolsaLaboralApplication.class, args);
+    }
+
+}
